17f459aa79f444a8343079f1c3dbed611f96b7d8
thach3112003 <thach3112003@gmail.com>
2025-03-18 14:34:45 +0700

    Push

---
+++ du-an-backend-python-django/spotifyBackendProject/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
+
+class MessageListCreate(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
